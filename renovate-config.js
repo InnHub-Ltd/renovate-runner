b288bd3c6e76b98c3bbd486d6d02cc5fbe2faa41
Opliko <opliko.reg@protonmail.com>
2023-05-11 00:13:51 +0200

    feat: initial renovate config

---
+++ renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branchPrefix: 'test-renovate/',
+  username: 'innhub-self-hosted-renovate[bot]',
+  gitAuthor: 'InnHub Self-Hosted Renovate <133168915+innhub-self-hosted-renovate[bot]@users.noreply.github.com>',
+  onboarding: false,
+  platform: 'github',
+  includeForks: true,
+  dryRun: 'full',
+  autodiscover: true,
+};
